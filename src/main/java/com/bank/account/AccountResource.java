829f9cc7a3c99f7cdcbfc476a1bc0dba954eef85
aliyan.mujahid <aliyan.mujahid@utopia.pk>
2025-11-18 17:25:54 +0500

    transfer EP corrected

--- src/main/java/com/bank/account/AccountResource.java
+++ src/main/java/com/bank/account/AccountResource.java
@@ -5,7 +5,6 @@ import com.bank.account.dto.TransferDTO;
 import com.bank.account.kafkaClients.AccountServiceProducer;
 import com.bank.account.model.Account;
 import com.bank.account.dto.AccountDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.ws.rs.core.Response;
 import jakarta.inject.Inject;
@@ -24,8 +23,6 @@ public class AccountResource {
 
     @Inject
     AccountServiceProducer accountServiceProducer;
-
-    private ObjectMapper mapper = new ObjectMapper();
 
     @GET
     public List<Account> getAll() {
@@ -55,24 +52,19 @@ public class AccountResource {
     // ---- Payment Transfer Endpoint ----
     @PUT
     @Path("/{accNo}/transfer")
-    @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
-    public Response transfer(@PathParam("accNo") String fromAccount,  @RequestBody TransferDTO dto) throws Exception {
+    public Response transfer(@PathParam("accNo") String fromAccount, TransferDTO dto) throws Exception {
+        System.out.println("Body: " + dto);
+
         Account acc = Account.find("accountNumber", fromAccount).firstResult();
-        if (acc == null) throw new NotFoundException("From Account not found");
+        if (acc == null) throw new NotFoundException("Incorrect Sender Account (not found)");
 
         if (acc.balance < dto.amount)
             throw new BadRequestException("Insufficient funds");
 
         // Tentative deduction
         acc.balance -= dto.amount;
-
-//        Account toAccount = Account.find("accountNumber", dto.toAccount).firstResult();
-//        if (toAccount == null) throw new NotFoundException("No Account found To Deposit");
-//        toAccount.balance += dto.amount;
-
         acc.persist();
-//        toAccount.persist();
 
         // Emit Kafka event
         TransactionEvent event = new TransactionEvent();
